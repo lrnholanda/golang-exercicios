41fad5ab4292bd2e264647155f8894995350dc93
lrnholanda <lrnholanda@gmail.com>
2023-07-04 20:13:08 -0300

    outros exercicios

--- struct_anonimo/main.go
+++ struct_anonimo/main.go
@@ -4,14 +4,40 @@ import (
 	"fmt"
 )
 
+type Pessoa struct {
+	nome      string
+	sobrenome string
+	sabores   []string
+}
+
 func main() {
-	x := struct {
-		nome  string
-		idade int
-	}{
-		nome:  "Mario",
-		idade: 50,
+	// x := struct {
+	// 	nome  string
+	// 	idade int
+	// }{
+	// 	nome:  "Mario",
+	// 	idade: 50,
+	// }
+
+	pessoa01 :=
+		Pessoa{
+			nome:      "Lorena",
+			sobrenome: "Holanda",
+			sabores:   []string{"romeu e julieta", "chocolate", "creme", "napolitano"}}
+
+	pessoa02 :=
+		Pessoa{
+			nome:      "Tetê",
+			sobrenome: "Holanda",
+			sabores:   []string{"romeu e julieta", "flocos", "creme", "napolitano"}}
+
+	fmt.Println(pessoa01.nome, pessoa01.sobrenome)
+	for _, value := range pessoa01.sabores {
+		fmt.Println("\t", value)
 	}
 
-	fmt.Println(x)
+	fmt.Println(pessoa02.nome, pessoa02.sobrenome)
+	for _, value := range pessoa02.sabores {
+		fmt.Println("\t", value)
+	}
 }
